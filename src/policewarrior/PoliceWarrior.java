02fd1ecc4dbbd94f45cdb8276ec4f5b15f7c5afd
mic-hb <michael.h22@mhs.istts.ac.id>
2023-05-15 14:36:55 +0700

    Coba-coba

--- src/policewarrior/PoliceWarrior.java
+++ src/policewarrior/PoliceWarrior.java
@@ -15,6 +15,7 @@ public class PoliceWarrior {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Kontol lele");
     }
     
 }
