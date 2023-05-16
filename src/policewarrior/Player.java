e3bc72c1350d5a825c8506da59ad3fd06ee22da8
mic-hb <michael.h22@mhs.istts.ac.id>
2023-05-16 15:26:24 +0700

    Template class Main & Player untuk Processing, membuat background dan animasi sprite

--- src/policewarrior/Player.java
+++ src/policewarrior/Player.java
@@ -1,5 +1,33 @@
 package policewarrior;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+
 public class Player extends Entity {
     protected String nama;
+    protected PImage[] idle;
+    protected int t = 0;
+    protected int timing = 3;
+    protected int total_frame = 18;
+    protected int reset_frame = -1;
+
+    public Player(PImage[] idle, int x, int y) {
+        this.idle = idle;
+        this.x = x;
+        this.y = y;
+    }
+
+    public void drawIdle(PApplet app, int f){
+        if(f == reset_frame){
+            t = 0;
+            f = 0;
+        }
+        if(f % timing == 0){
+            t++;
+        }
+        if(t > total_frame - 1){
+            t = 0;
+        }
+        app.image(idle[t], x, y);
+    }
 }
--- src/policewarrior/PoliceWarrior.java
+++ src/policewarrior/PoliceWarrior.java
@@ -4,18 +4,60 @@
  */
 package policewarrior;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+
 /**
  *
  * @author Michael
  */
-public class PoliceWarrior {
+public class PoliceWarrior extends PApplet {
+    /* Default */
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    public static final int FPS = 60;
+
+    /* Untuk animasi | processing grafik */
+    PImage bg;
+    Player p;
+    int f = 0;
+    private boolean idle = true;
+    private boolean running = false;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Kontol lele");
+        PApplet.main("contohgame.ContohGame");
     }
-    
+
+    public void settings(){
+        size(WIDTH, HEIGHT);
+    }
+
+    public void setup(){
+        // inisialisasi objek, seperti karakter, player, npc, menu, image, dll.
+        bg = loadImage("src/assets/background/background_1.png");
+        PImage[] temp = new PImage[18];
+        frameRate(FPS);
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = loadImage("src/assets/player/Idle_Blinking/(" + (i+1) + ").png");
+        }
+        p = new Player(temp, 100, 100);
+    }
+
+    public void draw(){
+        background(bg);
+        p.drawIdle(this, f);
+        f++;
+    }
+
+    public void keyPressed(){
+//        if(key == 'w') { up = true; running = true; idle = false}
+    }
+
+    public void keyRelease(){
+
+    }
 }
